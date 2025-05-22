d8799f459c6f72341e8f68e050efcc41ff0745af
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-22 16:53:48 +0200

    created Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className='flex items-center justify-between p-4 bg-[#8D6E63] text-[#FAF7F7] text-2xl'>
+      <div>
+        <h1 className='text-2xl text-[#FAF7F7] font-bold'>Zipelas Portfolio</h1>
+      </div>
+    </header>
+  );
+}
